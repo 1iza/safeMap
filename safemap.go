55d3207b3a221b21c7847b8b0a3cf8c619fd8037
lizian <lizian@yy.com>
2020-02-26 18:52:19 +0800

    dev

--- safemap.go
+++ safemap.go
@@ -6,26 +6,6 @@ import (
 	"reflect"
 )
 
-type opStruct struct {
-	key   interface{}
-	value interface{}
-	op    SAFEMAP_OP
-}
-
-func (x *opStruct) Key() interface{}   { return x.key }
-func (x *opStruct) Value() interface{} { return x.value }
-func (x *opStruct) Op() SAFEMAP_OP     { return x.op }
-
-type getStruct struct {
-	key   interface{}
-	value chan interface{}
-	op    SAFEMAP_OP
-}
-
-func (x *getStruct) Key() interface{}   { return x.key }
-func (x *getStruct) Value() interface{} { return x.value }
-func (x *getStruct) Op() SAFEMAP_OP     { return x.op }
-
 type safeMap struct {
 	ctx        context.Context
 	cancelFunc context.CancelFunc
